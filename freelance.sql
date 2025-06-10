ca242277ed5f825232eb33e855f1a12581810540
borisnoah <borisnoah91pgmail.com>
2025-06-10 17:16:42 +0100

    eojut de donnees dans bd noah

---
+++ freelance.sql
@@ -0,0 +1,81 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1:3306
+-- Généré le : mar. 10 juin 2025 à 14:19
+-- Version du serveur : 9.1.0
+-- Version de PHP : 8.3.14
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `freelance`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `prestataire`
+--
+
+DROP TABLE IF EXISTS `prestataire`;
+CREATE TABLE IF NOT EXISTS `prestataire` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `prenom` varchar(255) NOT NULL,
+  `telephone` varchar(20) DEFAULT NULL,
+  `email` varchar(191) NOT NULL,
+  `mot_de_passe` varchar(255) NOT NULL,
+  `adresse` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+
+-- Déchargement des données de la table `prestataire`
+INSERT INTO `prestataire` (`id`, `nom`, `prenom`, `telephone`, `email`, `mot_de_passe`, `adresse`) VALUES
+(1, 'Ngongo', 'Josephine', '689862222', 'maevanoubi21@gmail.com', 'Josephinengongo', 'Mokolo-Yaounde'),
+(2, 'noah', 'boris', '690112233', 'borisnoah91@gmail.com', 'niro2506', 'Bastos-Yaounde'),
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `proposant`
+--
+
+DROP TABLE IF EXISTS `proposant`;
+CREATE TABLE IF NOT EXISTS `proposant` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` varchar(255) NOT NULL,
+  `telephone` varchar(20) DEFAULT NULL,
+  `email` varchar(191) NOT NULL,
+  `mot_de_passe` varchar(255) NOT NULL,
+  `adresse` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Déchargement des données de la table `proposant`
+--
+
+INSERT INTO `proposant` (`id`, `nom`, `telephone`, `email`, `mot_de_passe`, `adresse`) VALUES
+(1, 'ELECTRA SHOPPING', '265378835', 'electrashopping@example.com', 'electra1', 'Nkolbisson-Yaounde'),
+(2, 'Kaithlyn KK', '698098325', 'kaithlynkk@gmail.com', 'nourriture123', 'Akwa-Douala');
+(10, 'Nana', 'Christian', '692334455', 'christian.nana@gmail.com', 'nanaChris', 'Biyem-Assi-Yaounde');
+
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
